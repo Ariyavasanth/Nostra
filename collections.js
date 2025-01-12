c07828b4acaa15ce457543e0a1281a2d64f563c6
Ariyavasanth <ariyavasanth@gmail.com>
2025-01-12 09:35:18 +0530

    added changes

---
+++ collections.js
@@ -0,0 +1,21 @@
+let close = document.getElementById("close")
+let offer_section = document.getElementById("offer-section")
+
+close.addEventListener("click", function(){
+    offer_section.style.display = "none"
+})
+
+//side nav bar
+let menu = document.getElementById("menu")
+let side_nav_close = document.getElementById("side-nav-close")
+let side_navbar = document.getElementById("side-navbar")
+
+
+menu.addEventListener("click",function(){
+    side_navbar.style.left = "0"
+})
+
+side_nav_close.addEventListener("click", () =>{
+    side_navbar.style.left = "-60%"
+
+})
